1777baf4f481f59d40829965a5d3a37f62a8ce06
moethu <m>
2020-05-27 07:57:49 +0200

    adding console example app

---
+++ SketchUpNETConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SketchUpNETConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SketchUpNET.SketchUp skp = new SketchUpNET.SketchUp();
+            if (skp.LoadModel(args[1]))
+            {
+
+            }
+        }
+    }
+}
